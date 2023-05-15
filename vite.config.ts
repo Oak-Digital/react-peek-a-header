b3df84f156d30b69139598941bdb70e2b021485e
Alexander Nortung <alexander.nortung@oakdigital.dk>
2023-05-15 15:47:13 +0200

    Updated build stuff to use vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,45 @@
+import path from 'path';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import packageJson from './package.json';
+import dts from "vite-plugin-dts";
+
+const getPackageName = () => {
+    return packageJson.name.split('/').pop() ?? packageJson.name;
+};
+
+const getPackageNameCamelCase = () => {
+    try {
+        return getPackageName().replace(/-./g, (char) => char[1].toUpperCase());
+    } catch (err) {
+        throw new Error('Name property in package.json is missing.');
+    }
+};
+
+const fileName = {
+    es: `main.mjs`,
+    cjs: `main.cjs`,
+    iife: `main.iife.js`,
+};
 
 export default defineConfig({
     /* root: 'demo', */
-    plugins: [react()],
+    plugins: [
+        react(),
+        dts({
+            insertTypesEntry: true,
+        }),
+    ],
     build: {
-        outDir: 'demo-dist',
+        outDir: process.env.BUILD === 'demo' ? 'demo-dist' : 'dist',
+        lib:
+            process.env.BUILD === 'demo'
+                ? undefined
+                : {
+                    entry: path.resolve(__dirname, 'src/main.ts'),
+                    name: getPackageNameCamelCase(),
+                    formats: ['es', 'cjs', 'iife'],
+                    fileName: (format) => fileName[format],
+                },
     },
 });
